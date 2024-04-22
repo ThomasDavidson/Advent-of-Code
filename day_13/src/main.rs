2e0d32840a0c911a0bdad401e93ffc8f84b0e8d6
ThomasDavidson <Tdavidson7992@conestogac.on.ca>
2024-04-21 22:08:40 -0400

    day 13: add parsing

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -1,3 +1,28 @@
+use std::char;
+
+#[derive(Debug)]
+struct Valley {
+    land: Vec<char>,
+}
+// -> Vec<Valley>
+fn parse_input(input: &str) -> Vec<Valley> {
+    let lines: Vec<&str> = input.lines().collect();
+
+    let mut valleys: Vec<Valley> = Vec::new();
+
+    for valley in lines.split(|&a| a == "") {
+        valleys.push(Valley {
+            land: valley.iter().flat_map(|&s| s.chars()).collect(),
+        });
+    }
+    valleys
+}
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../example.txt");
+
+    for line in input.lines() {
+        println!("{:?}", line);
+    }
+
+    let valleys = parse_input(input);
 }
