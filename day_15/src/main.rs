1af8fcdbd6690b0a6e1bc0c42a785402975be16c
ThomasDavidson <Tdavidson7992@conestogac.on.ca>
2024-04-23 11:50:19 -0400

    day 15: part 1

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,3 +1,24 @@
+use std::{ops::Rem, time::Instant};
+
+fn hash_algorithm(string: &str) -> usize {
+    string
+        .chars()
+        .fold(0, |acc, c| ((acc + c as usize) * 17).rem(256))
+}
+
+fn part_1(strings: &Vec<&str>) -> usize {
+    strings
+        .iter()
+        .fold(0, |acc, string| acc + hash_algorithm(string))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../example.txt");
+
+    let strings: Vec<&str> = input.split(",").collect();
+
+    let start: Instant = Instant::now();
+
+    let part_1_answer = part_1(&strings);
+
 }
