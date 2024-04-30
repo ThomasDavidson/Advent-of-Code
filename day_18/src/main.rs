7c769cbdfe9d2114a9cfe9608662f0f61687cc41
ThomasDavidson <Tdavidson7992@conestogac.on.ca>
2024-04-30 13:21:14 -0400

    day 18: part 2

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -31,6 +31,36 @@ fn part_1_decode_instructions(input: &str) -> Vec<Instruction> {
         .collect()
 }
 
+fn part_2_decode_instructions(input: &str) -> Vec<Instruction> {
+    input
+        .lines()
+        .map(|line| {
+            let split: Vec<&str> = line.split(['(', ')']).collect();
+            // let hex_str: Vec<char> = split[1].chars().collect();
+            // let array_of_chars: [char; 7] = hex_str.try_into().unwrap();
+            let instruction_str = split[1].to_string();
+
+            let length_str = &instruction_str[1..6];
+            let length: usize = match usize::from_str_radix(length_str, 16) {
+                Ok(a) => a,
+                Err(e) => panic!("Parse error: {:?}", e),
+            };
+            let direction_str = &instruction_str[6..7];
+            let direction = match direction_str {
+                "0" => Direction::East,
+                "1" => Direction::South,
+                "2" => Direction::West,
+                "3" => Direction::North,
+                a => panic!("Invalid hex code {a} {:?}", direction_str),
+            };
+
+            Instruction{
+                length: length,
+                direction: direction,
+            }
+        })
+        .collect()
+}
 
 fn calculate_area_from_instructions(instructions: Vec<Instruction>) -> u64 {
     let mut block_coord: Vec<(i64, i64)> = Vec::new();
@@ -65,7 +95,6 @@ fn calculate_area_from_instructions(instructions: Vec<Instruction>) -> u64 {
         .collect();
 
     let area: Area = block_coord.into();
-    println!("area {:?}", area);
 
     let inside_area: u64 = (area.0 as i64 - (b as i64) / 2 + 1).try_into().unwrap();
     inside_area + b as u64
@@ -76,8 +105,13 @@ fn part_1(input: &str) -> u64 {
     calculate_area_from_instructions(instructions)
 }
 
+fn part_2(input: &str)  -> u64 {
+    let instructions = part_2_decode_instructions(input);
+    calculate_area_from_instructions(instructions)
+}
+
 fn main() {
-    let input = include_str!("../example.txt");
+    let input = include_str!("../input.txt");
 
     let part_1_answer = part_1(input);
     println!("part 1 answer: {}", part_1_answer);
