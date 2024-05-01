8a3fd925d3f031f248148bd245e3374fb9687132
ThomasDavidson <Tdavidson7992@conestogac.on.ca>
2024-05-01 14:28:25 -0400

    day 19: initial part 2

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 #[derive(Debug, Clone, Eq, PartialEq)]
 enum WorkflowResult {
     Accept(bool),
@@ -134,7 +136,7 @@ fn parse_ratings_input(ratings_str: &str) -> Vec<Ratings> {
         .collect()
 }
 
-fn check_machine_part(rating: &Ratings, workflows: &Vec<Workflow>) -> Option<Ratings> {
+fn check_machine_part(rating: &Ratings, workflows: &Vec<Workflow>) -> bool {
     let mut workflow_result: WorkflowResult = WorkflowResult::Workflow("in".to_string());
 
     while !(workflow_result == WorkflowResult::Accept(true)
@@ -165,30 +167,123 @@ fn check_machine_part(rating: &Ratings, workflows: &Vec<Workflow>) -> Option<Rat
         }
     }
     match workflow_result {
-        WorkflowResult::Accept(true) => return Some(rating.clone()),
-        _ => return None,
+        WorkflowResult::Accept(true) => return true,
+        _ => return false,
     }
+}
+
+fn check_catagory_range(
+    range: Range<usize>,
+    range_catagory: char,
+    workflows: &Vec<Workflow>,
+    workflow_result: &WorkflowResult,
+) -> Vec<usize> {
+    let mut accepted_scores: Vec<usize> = Vec::new();
+
+    let Some(selected_workflow) = workflows
+        .iter()
+        .find(|&a| &WorkflowResult::Workflow(a.label.clone()) == workflow_result)
+    else {
+        panic!("label not found");
+    };
+
+    let mut current_range = range.clone();
+
+    for rule in &selected_workflow.workflow_rule {
+        let (mut result_range, next_range): (Vec<usize>, Range<usize>) = match rule.rule {
+            WorkflowCmp::Fallthrough => (current_range.clone().collect(), 0..0),
+            WorkflowCmp::Threshold(threshold, catagory, greater) => {
+                if catagory != range_catagory && !current_range.contains(&threshold) {
+                    continue;
+                }
+                // println!("{:?}", current_range);
+
+                let max_range = current_range.clone().max().unwrap();
+                let min_range = current_range.clone().min().unwrap();
+
+                let (accepted_range, regected_range) = match greater {
+                    // only used Range to match arms
+                    true => ((threshold + 1)..(max_range + 1), min_range..(threshold + 1)),
+                    false => (min_range..threshold, threshold..(max_range + 1)),
+                };
+                let accepted_vec = match rule.result {
+                    // todo add other range to accept condition
+                    WorkflowResult::Accept(true) => {
+                        println!("Accept");
+                        accepted_range.collect()
+                    }
+                    WorkflowResult::Accept(false) => {
+                        println!("Reject");
+                        accepted_range.collect()
+                    }
+                    WorkflowResult::Workflow(_) => check_catagory_range(
+                        accepted_range,
+                        range_catagory,
+                        workflows,
+                        &rule.result,
+                    ),
+                };
+                (accepted_vec, regected_range)
+            }
+        };
+        println!("len {:?} {:?}", result_range.len(), rule);
+        accepted_scores.append(&mut result_range);
+        // match (&rule.result, &rule.rule) {
+        //     (WorkflowResult::Accept(true), WorkflowCmp::Fallthrough) => {
+        //         accepted_scores.append(&mut result_range);
+        //     }
+        //     (_, _) => (),
+        // }
+        current_range = next_range;
+    }
+
+    println!("{:?} => {}", range, accepted_scores.len());
+    accepted_scores
 }
 
 fn part_1(ratings: &Vec<Ratings>, workflows: &Vec<Workflow>) -> usize {
     ratings
         .iter()
         .map(|rating| match check_machine_part(rating, workflows) {
-            None => 0,
-            Some(valid_rating) => valid_rating.get_sum(),
+            false => 0,
+            true => rating.get_sum(),
         })
         .fold(0, |acc, res| acc + res)
 }
 
+fn part_2(workflows: &Vec<Workflow>) -> usize {
+    let range = 1..(4000 + 1);
+
+    let initial = WorkflowResult::Workflow("in".to_string());
+
+    let accepted_x = check_catagory_range(range.clone(), 'x', workflows, &initial).len();
+    let accepted_m = check_catagory_range(range.clone(), 'm', workflows, &initial).len();
+    let accepted_a = check_catagory_range(range.clone(), 'a', workflows, &initial).len();
+    let accepted_s = check_catagory_range(range.clone(), 's', workflows, &initial).len();
+
+    println!("accepted_x {accepted_x} accepted_m {accepted_m} accepted_a {accepted_a} accepted_s {accepted_s}");
+
+    vec![accepted_x, accepted_m, accepted_a, accepted_s]
+        .iter()
+        .fold(0, |acc, i| acc + i)
+}
+
 fn main() {
-    let input = include_str!("../input.txt");
+    let input = include_str!("../example.txt");
 
     let (workflows_str, ratings_str) = input.split_once("\r\n\r\n").unwrap();
 
     let workflows = parse_workflows(workflows_str);
 
+    // for workflow in &workflows {
+    //     println!("{:#?}", workflow);
+    // }
+
     let ratings = parse_ratings_input(ratings_str);
 
     let part_1_score = part_1(&ratings, &workflows);
     println!("Result: {part_1_score}");
+
+    let part_2_score = part_2(&workflows);
+    println!("Result: {part_2_score}");
 }
