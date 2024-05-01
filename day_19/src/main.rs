5b7e4a27817fa5ccb5184da9f23f1fecb0266608
ThomasDavidson <Tdavidson7992@conestogac.on.ca>
2024-05-01 10:21:54 -0400

    day 19: organize code

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -51,28 +51,24 @@ fn parse_workflows(workflows_str: &str) -> Vec<Workflow> {
     let mut workflows: Vec<Workflow> = Vec::new();
 
     for line in workflows_str.lines() {
-        let mut split = line.split(['{', '}', ',']);
+        let mut split = line.split(['{', '}', ',']).filter(|a| !a.is_empty());
+        // label is allways the first split item
         let label = split.next().unwrap();
         let mut workflow = Workflow {
             label: label.to_string(),
             workflow_rule: Vec::new(),
         };
 
-        // println!("Label: {label}");
         for rule in split {
-            // println!("{rule}");
             let rule_split = rule.split(":").collect::<Vec<&str>>();
-            if rule.is_empty() {
-                continue;
-            }
 
-            // println!("{:?}", rule_split);
             match rule_split.len() {
-                // fallthrough
+                // Fallthrough
                 1 => workflow.workflow_rule.push(WorkflowRule {
                     result: WorkflowResult::parse(rule_split[0]),
                     rule: WorkflowCmp::Fallthrough,
                 }),
+                // Threshold
                 2 => workflow.workflow_rule.push(WorkflowRule {
                     result: WorkflowResult::parse(rule_split[1]),
                     rule: WorkflowCmp::parse(rule_split[0]),
@@ -85,7 +81,7 @@ fn parse_workflows(workflows_str: &str) -> Vec<Workflow> {
     workflows
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Ratings {
     x: usize,
     m: usize,
@@ -126,6 +122,9 @@ impl Ratings {
             _ => panic!("invalid char {c}"),
         }
     }
+    fn get_sum(&self) -> usize {
+        self.x + self.m + self.a + self.s
+    }
 }
 
 fn parse_ratings_input(ratings_str: &str) -> Vec<Ratings> {
@@ -135,9 +134,54 @@ fn parse_ratings_input(ratings_str: &str) -> Vec<Ratings> {
         .collect()
 }
 
+fn check_machine_part(rating: &Ratings, workflows: &Vec<Workflow>) -> Option<Ratings> {
+    let mut workflow_result: WorkflowResult = WorkflowResult::Workflow("in".to_string());
+
+    while !(workflow_result == WorkflowResult::Accept(true)
+        || workflow_result == WorkflowResult::Accept(false))
+    {
+        let Some(selected_workflow) = workflows
+            .iter()
+            .find(|&a| WorkflowResult::Workflow(a.label.clone()) == workflow_result)
+        else {
+            panic!("label not found");
+        };
+
+        for rule in &selected_workflow.workflow_rule {
+            let result = match rule.rule {
+                WorkflowCmp::Fallthrough => true,
+                WorkflowCmp::Threshold(threshold, catagory, greater) => match greater {
+                    true => rating.get_value(catagory) < threshold,
+                    false => rating.get_value(catagory) > threshold,
+                },
+            };
+            match (result, rule.result.clone()) {
+                (true, _) => {
+                    workflow_result = rule.result.clone();
+                    break;
+                }
+                (false, _) => (),
+            }
+        }
+    }
+    match workflow_result {
+        WorkflowResult::Accept(true) => return Some(rating.clone()),
+        _ => return None,
+    }
+}
+
+fn part_1(ratings: &Vec<Ratings>, workflows: &Vec<Workflow>) -> usize {
+    ratings
+        .iter()
+        .map(|rating| match check_machine_part(rating, workflows) {
+            None => 0,
+            Some(valid_rating) => valid_rating.get_sum(),
+        })
+        .fold(0, |acc, res| acc + res)
+}
+
 fn main() {
     let input = include_str!("../input.txt");
-    let mut part_1_score = 0;
 
     let (workflows_str, ratings_str) = input.split_once("\r\n\r\n").unwrap();
 
@@ -145,41 +189,6 @@ fn main() {
 
     let ratings = parse_ratings_input(ratings_str);
 
-    for rating in ratings {
-        let mut workflow_result: WorkflowResult = WorkflowResult::Workflow("in".to_string());
-
-        while !(workflow_result == WorkflowResult::Accept(true)
-            || workflow_result == WorkflowResult::Accept(false))
-        {
-            let Some(selected_workflow) = workflows
-                .iter()
-                .find(|&a| WorkflowResult::Workflow(a.label.clone()) == workflow_result)
-            else {
-                panic!("label not found");
-            };
-
-            for rule in &selected_workflow.workflow_rule {
-                let result = match rule.rule {
-                    WorkflowCmp::Fallthrough => true,
-                    WorkflowCmp::Threshold(threshold, catagory, greater) => match greater {
-                        true => rating.get_value(catagory) < threshold,
-                        false => rating.get_value(catagory) > threshold,
-                    },
-                };
-                match (result, rule.result.clone()) {
-                    (true, _) => {
-                        workflow_result = rule.result.clone();
-                        break;
-                    }
-                    (false, _) => (),
-                }
-            }
-        }
-        match workflow_result {
-            WorkflowResult::Accept(true) => part_1_score += rating.x + rating.m + rating.a + rating.s,
-            _ => (),
-        }
-    }
-
+    let part_1_score = part_1(&ratings, &workflows);
     println!("Result: {part_1_score}");
 }
