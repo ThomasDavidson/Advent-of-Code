d23d71bd9c04e0b00747bdcec8ad249a3b4b455c
ThomasDavidson <thomasdavidson268@gmail.com>
2024-10-24 16:23:31 -0400

    day 21: start part 2

--- day_21/src/main.rs
+++ day_21/src/main.rs
@@ -1,7 +1,7 @@
 use crate::Tile::{GardenPlot, Rocks, Start};
 use colored::Colorize;
 use library::grid::Direction;
-use library::math::sawtooth;
+use library::math::{round_to, sawtooth};
 use std::{
     collections::{HashMap, VecDeque},
     fmt,
@@ -104,6 +104,93 @@ impl Garden {
 
     fn get_step_coords(&self) -> impl Iterator<Item = &u32> {
         self.steps.values()
+    }
+
+    fn get_steps_range(
+        &self,
+        y_max: i64,
+        y_min: i64,
+        x_max: i64,
+        x_min: i64,
+    ) -> impl Iterator<Item = &u32> {
+        let entries = self
+            .steps
+            .iter()
+            .filter(move |(k, _v)| k.1 < y_max && k.1 >= y_min && k.0 < x_max && k.0 >= x_min);
+
+        entries.map(|x| x.1)
+    }
+    fn print_steps_range(&self, y_max: i64, y_min: i64, x_max: i64, x_min: i64) {
+        let entries: Vec<((i64, i64), u32)> = self
+            .steps
+            .clone()
+            .drain()
+            .filter(move |(k, _v)| k.1 < y_max && k.1 >= y_min && k.0 < x_max && k.0 >= x_min)
+            .collect();
+
+        println!("{:?}", entries)
+    }
+
+    fn calculate_score_for_each_universe(&self, max_steps: u32) -> Vec<Vec<u64>> {
+        let width = self.width();
+        let height = self.height();
+
+        let steps: Vec<&(i64, i64)> = self.steps.keys().collect::<Vec<_>>();
+
+        let min_x_visited = steps.iter().min_by_key(|f| f.0).unwrap().0;
+        let min_y_visited = steps.iter().min_by_key(|f| f.1).unwrap().1;
+        let max_x_visited = steps.iter().max_by_key(|f| f.0).unwrap().0;
+        let max_y_visited = steps.iter().max_by_key(|f| f.1).unwrap().1;
+
+        let min_x_step = round_to(min_x_visited, -(width as i64));
+        let min_y_step = round_to(min_y_visited, -(height as i64));
+        let max_x_step = round_to(max_x_visited, width as i64);
+        let max_y_step = round_to(max_y_visited, height as i64);
+
+        for x in (min_x_step..max_x_step).step_by(width) {
+            for y in (min_y_step..max_y_step).step_by(height) {
+                let steps = self.get_steps_range(y + height as i64, y as i64, x + width as i64, x);
+                let score = self.calculate_score_from_iter(steps, max_steps);
+                print!("{score}\t");
+                if score == 27 {
+                    self.print_steps_range(y + height as i64, y as i64, x + width as i64, x);
+                }
+            }
+            println!()
+        }
+
+        let us: Vec<Vec<u64>> = (min_x_step..max_x_step)
+            .step_by(width)
+            .map(|x| {
+                (min_y_step..max_y_step)
+                    .step_by(height)
+                    .map(|y| {
+                        let steps =
+                            self.get_steps_range(y + height as i64, y as i64, x + width as i64, x);
+                        self.calculate_score_from_iter(steps, max_steps)
+                    })
+                    .collect()
+            })
+            .collect();
+
+        us
+    }
+
+    fn calculate_score_from_iter<'a>(
+        &self,
+        iter: impl Iterator<Item = &'a u32>,
+        max_steps: u32,
+    ) -> u64 {
+        iter.map(|step| {
+            // even coordinates can only be reached with an even amount of steps an vise versa with odd steps
+            let even_coord = step % 2 == 0;
+            let even_max_step = max_steps % 2 == 0;
+            match even_coord == even_max_step {
+                true => 1,
+                false => 0,
+            }
+        })
+        .fold(0, |acc, x| acc + x)
     }
 
     fn calculate_score(&self, max_steps: u32) -> u64 {
@@ -243,6 +330,108 @@ fn part_1(input: &str, max_steps: u32) -> i32 {
     garden.calculate_score(max_steps)
 }
 
+fn part_2(input: &str, max_steps: u32) -> u64 {
+    let mut garden = Garden::from_string(input);
+
+    let width = garden.width() as i64;
+    let height = garden.height() as i64;
+    if width != height {
+        panic!("Not sqare");
+    }
+
+    let reduced_max_steps = if max_steps > width as u32 * 5 {
+        (max_steps as i64 % width + width * 2) as u32
+    } else {
+        panic!();
+    };
+
+    println!("steps: {max_steps}, reduced steps: {reduced_max_steps}");
+
+    let gardener = Elf {
+        max_steps: reduced_max_steps,
+        steps: 0,
+        coords: garden.find_start(),
+    };
+
+    let mut gardeners = VecDeque::from([gardener]);
+
+    while let Some(gardener) = gardeners.pop_front() {
+        if garden.visited(&gardener) {
+            continue;
+        }
+
+        garden.visit(&gardener);
+        let mut next = garden.find_next(gardener);
+        gardeners.append(&mut next);
+    }
+
+    let scores = garden.calculate_score_for_each_universe(max_steps);
+
+    let origin = if scores.len() == 5 { 2 } else { 3 };
+
+    println!("Origin: {origin}");
+
+    let e1 = scores[origin][origin];
+    let e2 = scores[origin - 1][origin];
+
+    println!("e: {e1}");
+    println!("e2: {e2}");
+
+    let a1 = scores[origin - 1][origin - 1];
+    let a2 = scores[origin - 1][origin + 1];
+    let a3 = scores[origin + 1][origin + 1];
+    let a4 = scores[origin + 1][origin - 1];
+    let a = a1 + a2 + a3 + a4;
+
+    let t1 = scores[origin - 2][origin];
+    let t2 = scores[origin][origin - 2];
+    let t3 = scores[origin + 2][origin];
+    let t4 = scores[origin][origin + 2];
+
+    // set 0 if d doesn't exist
+    let (d1, d2) = if scores.len() == 5 {
+        (0, 0)
+    } else {
+        let d1: u64 = scores[origin - 3][origin];
+        let d2 = scores[origin][origin - 3];
+        (d1, d2)
+    };
+
+    let (d3, d4) = if origin + 3 > scores.len() {
+        let d3 = scores[origin + 3][origin];
+        let d4 = scores[origin][origin + 3];
+        (d3, d4)
+    } else {
+        (0, 0)
+    };
+
+    let d = d1 + d2 + d3 + d4;
+
+    let t = t1 + t2 + t3 + t4;
+
+    let b1 = scores[origin - 1][origin - 2];
+    let b2 = scores[origin - 2][origin + 1];
+    let b3 = scores[origin + 1][origin - 2];
+    let b4 = scores[origin + 1][origin + 2];
+    let b = b1 + b2 + b3 + b4;
+
+    println!("a1: {a1}, a2: {a2}, a3: {a3}, a4: {a4}");
+    println!("t1: {t1}, t2: {t2}, t3: {t3}, t4: {t4}");
+    println!("b1: {b1}, b2: {b2}, b3: {b3}, b4: {b4}");
+    println!("d1: {d1}, d2: {d2}, d3: {d3}, d4: {d4}");
+
+    println!("a: {a}, b: {b}, t: {t}, d {d}");
+
+    let s = (width - 1) / 2;
+    let n = ((max_steps as i64 - s) / width) as u64;
+
+    println!("s: {s}, n: {n}");
+
+    // println!("n for max steps: {}", n);
+
+    (n - 1).pow(2) * e1 + n.pow(2) * e2 + (n - 1) * a + n * b + t + d
+}
+
 fn main() {
     let input = include_str!("../input.txt");
 
@@ -250,11 +439,16 @@ fn main() {
     let part_1_answer = part_1(&input, 64);
     let duration = start.elapsed();
     println!("Part 1 answer: {}, time: {:?}", part_1_answer, duration);
+
+    let start: Instant = Instant::now();
+    let part_2_answer = part_2(&input, 1000);
+    let duration = start.elapsed();
+    println!("Part 2 answer: {}, time: {:?}", part_2_answer, duration);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::part_1;
+    use crate::{part_1, part_2};
     #[test]
     fn test1() {
         let input = include_str!("../example.txt");
@@ -292,9 +486,27 @@ mod tests {
         assert_eq!(result, 668697);
     }
     #[test]
-    fn test7() {
+    fn test2_4() {
         let input = include_str!("../example.txt");
-        let result = part_1(input, 5000);
+        let result = part_2(input, 100);
+        assert_eq!(result, 6536);
+    }
+    #[test]
+    fn test2_5() {
+        let input = include_str!("../example.txt");
+        let result = part_2(input, 500);
+        assert_eq!(result, 167004);
+    }
+    #[test]
+    fn test2_6() {
+        let input = include_str!("../example.txt");
+        let result = part_2(input, 1000);
+        assert_eq!(result, 668697);
+    }
+    #[test]
+    fn test2_7() {
+        let input = include_str!("../example.txt");
+        let result = part_2(input, 5000);
         assert_eq!(result, 16733044);
     }
 }
