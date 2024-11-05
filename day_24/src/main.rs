575b62057ab5ccedd2b1bd279487f00f11a91436
ThomasDavidson <thomasdavidson268@gmail.com>
2024-11-05 10:01:11 -0500

    simplify collision code

--- day_24/src/main.rs
+++ day_24/src/main.rs
@@ -1,7 +1,7 @@
 use library::grid::Vec3;
 use std::{fs, time::Instant};
 
-const EXAMPLE: bool = true;
+const EXAMPLE: bool = false;
 const INPUT_FILE: &'static str = match EXAMPLE {
     true => "./example.txt",
     false => "./input.txt",
@@ -32,7 +32,7 @@ impl HailStone {
         (a, b, c)
     }
 
-    fn check_intersection(&self, other: &Self) -> Option<(f64, f64)> {
+    fn check_intersection_xy(&self, other: &Self) -> Option<(f64, f64)> {
         let (a1, b1, c1) = self.get_equation();
         let (a2, b2, c2) = other.get_equation();
 
@@ -42,6 +42,19 @@ impl HailStone {
             return None;
         }
         Some((x0, y0))
+    }
+
+    fn future_intersection_xy(&self, (x0, y0): (f64, f64)) -> bool {
+        // check if the intersection has already happened
+        let (dx, dy) = (x0 - self.position.x as f64, y0 - self.position.y as f64);
+
+        if (dy.is_sign_positive() != self.velocity.y.is_positive())
+            || (dx.is_sign_positive() != self.velocity.x.is_positive())
+        {
+            return false;
+        }
+
+        true
     }
 }
 
@@ -75,31 +88,16 @@ fn part_1(input: &str) -> u64 {
             .enumerate()
             .filter(|hs| hs.1 != hail_stone && min_j <= hs.0)
         {
-            let (x0, y0) = match hail_stone.check_intersection(hail_stone2) {
+            let (x0, y0) = match hail_stone.check_intersection_xy(hail_stone2) {
                 None => continue,
                 Some(xy0) => xy0,
             };
 
-            // check if the intersection has already happened
-            let (dx, dy) = (
-                x0 - hail_stone.position.x as f64,
-                y0 - hail_stone.position.y as f64,
-            );
-
-            if (dy.is_sign_positive() != hail_stone.velocity.y.is_positive())
-                || (dx.is_sign_positive() != hail_stone.velocity.x.is_positive())
-            {
+            if !hail_stone.future_intersection_xy((x0, y0)) {
                 continue;
             }
 
-            let (dx2, dy2) = (
-                x0 - hail_stone2.position.x as f64,
-                y0 - hail_stone2.position.y as f64,
-            );
-
-            if (dy2.is_sign_positive() != hail_stone2.velocity.y.is_positive())
-                || (dx2.is_sign_positive() != hail_stone2.velocity.x.is_positive())
-            {
+            if !hail_stone2.future_intersection_xy((x0, y0)) {
                 continue;
             }
 
@@ -133,7 +131,7 @@ mod tests {
         let hail_stone = HailStone::from_line(lines[0]);
         let hail_stone2 = HailStone::from_line(lines[1]);
 
-        let result = hail_stone.check_intersection(&hail_stone2);
+        let result = hail_stone.check_intersection_xy(&hail_stone2);
         assert_eq!(result, Some((14.0 + 1. / 3., 15. + 1. / 3.)));
     }
     #[test]
@@ -143,7 +141,7 @@ mod tests {
         let hail_stone = HailStone::from_line(lines[0]);
         let hail_stone2 = HailStone::from_line(lines[2]);
 
-        let result = hail_stone.check_intersection(&hail_stone2);
+        let result = hail_stone.check_intersection_xy(&hail_stone2);
         assert_eq!(result, Some((11.0 + 2. / 3., 16. + 2. / 3.)));
     }
     #[test]
@@ -153,7 +151,7 @@ mod tests {
         let hail_stone = HailStone::from_line(lines[0]);
         let hail_stone2 = HailStone::from_line(lines[3]);
 
-        let result = hail_stone.check_intersection(&hail_stone2);
+        let result = hail_stone.check_intersection_xy(&hail_stone2);
         assert_eq!(result, Some((6.2, 19.4)));
     }
     #[test]
@@ -163,7 +161,7 @@ mod tests {
         let hail_stone = HailStone::from_line(lines[1]);
         let hail_stone2 = HailStone::from_line(lines[2]);
 
-        let result = hail_stone.check_intersection(&hail_stone2);
+        let result = hail_stone.check_intersection_xy(&hail_stone2);
         assert_eq!(result, None);
     }
     #[test]
@@ -173,7 +171,7 @@ mod tests {
         let hail_stone = HailStone::from_line(lines[1]);
         let hail_stone2 = HailStone::from_line(lines[3]);
 
-        let result = hail_stone.check_intersection(&hail_stone2);
+        let result = hail_stone.check_intersection_xy(&hail_stone2);
         assert_eq!(result, Some((-6., -5.)));
     }
     #[test]
@@ -183,7 +181,7 @@ mod tests {
         let hail_stone = HailStone::from_line(lines[2]);
         let hail_stone2 = HailStone::from_line(lines[3]);
 
-        let result = hail_stone.check_intersection(&hail_stone2);
+        let result = hail_stone.check_intersection_xy(&hail_stone2);
         assert_eq!(result, Some((-2., 3.)));
     }
     #[test]
@@ -193,7 +191,7 @@ mod tests {
         let hail_stone = HailStone::from_line(lines[2]);
         let hail_stone2 = HailStone::from_line(lines[4]);
 
-        let result = hail_stone.check_intersection(&hail_stone2);
+        let result = hail_stone.check_intersection_xy(&hail_stone2);
         assert_eq!(result, Some((19., 24.)));
     }
 }
